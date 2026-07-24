4b1de3a68fe9059aff734a74f1606721b06da199
Sucre00 <iwutedivine08523@gmail.com>
2026-07-24 17:31:45 +0100

    Fix CORS policy header: allow dynamic Origin header and ALLOWED_ORIGIN env var for production frontend

--- backend/main.go
+++ backend/main.go
@@ -97,11 +97,20 @@ func main() {
 	// CORS wrapper
 	corsHandler := func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+			origin := r.Header.Get("Origin")
+			allowedOrigin := os.Getenv("ALLOWED_ORIGIN")
+			if allowedOrigin != "" {
+				w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
+			} else if origin != "" {
+				w.Header().Set("Access-Control-Allow-Origin", origin)
+			} else {
+				w.Header().Set("Access-Control-Allow-Origin", "*")
+			}
+
 			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
-			
+
 			if r.Method == "OPTIONS" {
 				w.WriteHeader(http.StatusOK)
 				return
